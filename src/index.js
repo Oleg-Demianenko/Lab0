7d3b1d011e9f8c33b3fba062ba0fc8d0797238c5
Oleg-Demianenko <92909188+Oleg-Demianenko@users.noreply.github.com>
2026-09-16 22:56:20 +0300

    Complited task for var. 6
    Function 4x - x^2 and midpoint rule integration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-alert("Let's integrate!")
+const mathFunction = (x) => 4 * x - Math.pow(x, 2);
+
+function MidpointRuleIntegration(func, a, b, n) 
+{
+    const deltaX = (b - a) / n;
+    let sum = 0;  
+    for (let i = 0; i < n; i++)
+        sum += func(a + (1 + 2 * i) * deltaX / 2);
+
+    sum *= deltaX;
+    return sum;
+}
+
+alert("Let's integrate!");
+
+const a = parseInt(prompt("a: "));
+const b = parseInt(prompt("b: "));
+const n = parseFloat(prompt("Количество точек интегрирования: "));
+
+const result = MidpointRuleIntegration(mathFunction, a, b, n);
+alert(` a: ${a}\n b: ${b}\n Точек: ${n}\n Результат: ${result}`);
+
